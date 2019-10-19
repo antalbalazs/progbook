e18249cf9670151f1dce422a569b42055283c1d9
raczandras <raczandras0204@gmail.com>
2019-10-19 12:45:29 +0200

    m

--- src/prog2/Stroustrup/boost.cpp
+++ src/prog2/Stroustrup/boost.cpp
@@ -1,93 +1,38 @@
-#include <vector>
-#include <algorithm>
+#include <iostream>
 #include <string>
+#include <fstream>
+#include <iomanip>
+#include <vector>
 #include <boost/filesystem.hpp>
- 
-using namespace boost::system;
-namespace filesys = boost::filesystem;
-#ifndef USING_BOOST
-#define USING_BOOST
-#endif
- 
-/*
- * Get the list of all files in given directory and its sub directories.
- *
- * Arguments
- * 	dirPath : Path of directory to be traversed
- * 	dirSkipList : List of folder names to be skipped
- *
- * Returns:
- * 	vector containing paths of all the files in given directory and its sub directories
- *
- */
-std::vector<std::string> getAllFilesInDir(const std::string &dirPath, 	const std::vector<std::string> dirSkipList = { })
+using namespace std;
+
+int db=0;
+
+void read_file (boost::filesystem::path path, vector<string> folders)
 {
- 
-	// Create a vector of string
-	std::vector<std::string> listOfFiles;
-	try {
-		// Check if given path exists and points to a directory
-		if (filesys::exists(dirPath) && filesys::is_directory(dirPath))
-		{
-			// Create a Recursive Directory Iterator object and points to the starting of directory
-			filesys::recursive_directory_iterator iter(dirPath);
- 
-			// Create a Recursive Directory Iterator object pointing to end.
-			filesys::recursive_directory_iterator end;
- 
-			// Iterate till end
-			while (iter != end)
-			{
-				// Check if current entry is a directory and if exists in skip list
-				if (filesys::is_directory(iter->path()) &&
-						(std::find(dirSkipList.begin(), dirSkipList.end(), iter->path().filename()) != dirSkipList.end()))
-				{
-					// Skip the iteration of current directory pointed by iterator
-#ifdef USING_BOOST
-					// Boost Fileystsem  API to skip current directory iteration
-					iter.no_push();
-#else
-					// c++17 Filesystem API to skip current directory iteration
-					iter.disable_recursion_pending();
-#endif
-				}
-				else
-				{
-					// Add the name in vector
-					listOfFiles.push_back(iter->path().string());
-				}
- 
-				error_code ec;
-				// Increment the iterator to point to next entry in recursive iteration
-				iter.increment(ec);
-				if (ec) {
-					std::cerr << "Error While Accessing : " << iter->path().string() << " :: " << ec.message() << '\n';
-				}
-			}
+	if(is_regular_file(path)){
+		string ext(".java");
+		if(!ext.compare(boost::filesystem::extension (path))) {
+			string file=path.string();
+			size_t end = file.find_last_of("/");
+			string folder = file.substr(0,end);
+
+ 			folders.push_back(folder);
+			cout << file << "\n";
+			db++;
 		}
-	}
-	catch (std::system_error & e)
-	{
-		std::cerr << "Exception :: " << e.what();
-	}
-	return listOfFiles;
-}
-int main()
+
+ 	}
+	else if(is_directory(path))
+ 		for( boost::filesystem::directory_entry & entry : boost::filesystem::directory_iterator (path))
+			 read_file(entry.path(), folders);
+ 	}
+
+int main( int argc, char *argv[])
 {
-	std::string dirPath = "/home/user/study";
- 
-	// Get recursive list of files in given directory and its sub directories
-	std::vector<std::string> listOfFiles = getAllFilesInDir(dirPath);
- 
-	// Iterate over the vector and print all files
-	for (auto str : listOfFiles)
-		std::cout << str << std::endl;
- 
-	std::cout << "**********************" << std::endl;
- 
-	// Get recursive list of files in given directory and skip given folders
-	listOfFiles = getAllFilesInDir(dirPath, { ".svn", "logs" });
- 
-	for (auto str : listOfFiles)
-		std::cout << str << std::endl;
+	string path="java";
+	vector<string> folders;
+	read_file(path, folders);
+	cout << "A JDK osztályainak a száma: " << db << "\n";
+
 }
